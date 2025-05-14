92b7c7df79b461d4e17131250484f428ddb76206
Ratthasart Rojjanai <feevfeev2@gmail.com>
2025-05-14 16:13:53 +0700

    up

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5500;
-app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}/frontend/main.html`));
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}/frontend/`));
 // app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}/`));
